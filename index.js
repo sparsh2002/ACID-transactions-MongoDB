473baf0042a1c745d918abd6b8dedd3652b6b1c8
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2022-06-14 10:32:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const app = express()
+
+dotenv.config()
